7c27bb416db8505e5cd3d0c27f113b22b2ec36b5
machearn <machearn.n@gmail.com>
2023-11-28 17:59:36 -0500

    dev: implement thread-safe blocking queue

---
+++ include/queue.hpp
@@ -0,0 +1,77 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+namespace web_server {
+namespace utils {
+
+template <typename T>
+class Queue {
+public:
+  Queue() = default;
+  Queue(const Queue<T>&) = delete;
+  Queue& operator=(const Queue<T>&) = delete;
+
+  Queue(Queue<T>&& other_queue) {
+    std::lock_guard<std::mutex> lock{_mutex};
+    _queue = std::move(other_queue._queue);
+  }
+
+  Queue<T>& operator=(Queue<T>&& other_queue) {
+    std::lock_guard<std::mutex> lock{_mutex};
+    _queue = std::move(other_queue._queue);
+    return *this;
+  }
+
+  ~Queue() = default;
+
+  void push(const T& value) {
+    std::lock_guard<std::mutex> lock{_mutex};
+    _queue.push(value);
+    _cv.notify_one();
+  }
+
+  void push(T&& value) {
+    std::lock_guard<std::mutex> lock{_mutex};
+    _queue.push(std::move(value));
+    _cv.notify_one();
+  }
+
+  T pop() {
+    std::unique_lock<std::mutex> lock{_mutex};
+    _cv.wait(lock, [this](){return !_queue.empty();});
+    T value = std::move(_queue.front());
+    _queue.pop();
+    return value;
+  }
+
+  bool empty() const {
+    std::lock_guard<std::mutex> lock{_mutex};
+    return _queue.empty();
+  }
+
+  std::size_t size() const {
+    std::lock_guard<std::mutex> lock{_mutex};
+    return _queue.size();
+  }
+
+  void clear() {
+    std::lock_guard<std::mutex> lock{_mutex};
+    while (!_queue.empty()) {
+      _queue.pop();
+    }
+  }
+
+private:
+  std::queue<T> _queue;
+  mutable std::mutex _mutex;
+  std::condition_variable _cv;
+};
+
+} // namespace utils
+} // namespace web_server
+
+#endif // QUEUE_H_
---
+++ test/queue_test.cpp
@@ -0,0 +1,112 @@
+#include "../include/queue.hpp"
+
+#include <gtest/gtest.h>
+#include <vector>
+#include <thread>
+
+class QueueTest: public testing::Test {
+protected:
+  web_server::utils::Queue<int> queue;
+};
+
+TEST_F(QueueTest, PushAndPop) {
+  queue.push(1);
+  queue.push(2);
+  queue.push(3);
+  EXPECT_EQ(queue.pop(), 1);
+  EXPECT_EQ(queue.pop(), 2);
+  EXPECT_EQ(queue.pop(), 3);
+}
+
+TEST_F(QueueTest, Empty) {
+  EXPECT_TRUE(queue.empty());
+  queue.push(1);
+  EXPECT_FALSE(queue.empty());
+  queue.pop();
+  EXPECT_TRUE(queue.empty());
+}
+
+TEST_F(QueueTest, Size) {
+  EXPECT_EQ(queue.size(), 0);
+  queue.push(1);
+  EXPECT_EQ(queue.size(), 1);
+  queue.push(2);
+  EXPECT_EQ(queue.size(), 2);
+  queue.push(3);
+  EXPECT_EQ(queue.size(), 3);
+  queue.pop();
+  EXPECT_EQ(queue.size(), 2);
+  queue.pop();
+  EXPECT_EQ(queue.size(), 1);
+  queue.pop();
+  EXPECT_EQ(queue.size(), 0);
+}
+
+TEST_F(QueueTest, MoveConstructor) {
+  queue.push(1);
+  queue.push(2);
+  queue.push(3);
+  web_server::utils::Queue<int> other_queue{std::move(queue)};
+  EXPECT_TRUE(queue.empty());
+  EXPECT_EQ(other_queue.size(), 3);
+  EXPECT_EQ(other_queue.pop(), 1);
+  EXPECT_EQ(other_queue.pop(), 2);
+  EXPECT_EQ(other_queue.pop(), 3);
+}
+
+TEST_F(QueueTest, MoveAssignment) {
+  queue.push(1);
+  queue.push(2);
+  queue.push(3);
+  web_server::utils::Queue<int> other_queue;
+  EXPECT_TRUE(other_queue.empty());
+  other_queue = std::move(queue);
+  EXPECT_TRUE(queue.empty());
+  EXPECT_EQ(other_queue.size(), 3);
+  EXPECT_EQ(other_queue.pop(), 1);
+  EXPECT_EQ(other_queue.pop(), 2);
+  EXPECT_EQ(other_queue.pop(), 3);
+}
+
+TEST_F(QueueTest, MultithreadPushAndPop) {
+  std::vector<std::thread> threads;
+
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back([this]() {
+      for (int j = 0; j < 100; j++) {
+        queue.push(j);
+      }
+    });
+  }
+
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back([this]() {
+      for (int j = 0; j < 100; j++) {
+        queue.pop();
+      }
+    });
+  }
+
+  for (auto& thread : threads) {
+    thread.join();
+  }
+
+  EXPECT_TRUE(queue.empty());
+}
+
+TEST_F(QueueTest, TwoThreadsPushAndPop) {
+  std::thread pop_thread{[this]() {
+    for (int i = 0; i < 1000; i++) {
+      queue.pop();
+    }
+  }};
+
+  for (int i = 0; i < 1000; i++) {
+    queue.push(i);
+  }
+
+  pop_thread.join();
+
+  EXPECT_TRUE(queue.empty());
+}
+
